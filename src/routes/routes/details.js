bbcb119e22e82effc39c2567bf3f9db691da1853
RodCarranza <93809998+RodCarranza@users.noreply.github.com>
2026-05-26 18:53:44 -0600

    Improve the operating schedule display - task

--- src/routes/routes/details.js
+++ src/routes/routes/details.js
@@ -1,11 +1,16 @@
-import { getRouteById, getSchedulesByRoute } from "../../models/model.js";
+import { getCompleteRouteDetails } from "../../models/model.js";
 
 export default async (req, res) => {
   const { routeId } = req.params;
-  const details = await getRouteById(routeId);
-  details.schedules = await getSchedulesByRoute(routeId);
 
-  // TODO: getCompleteRouteDetails instead
+  const details = await getCompleteRouteDetails(routeId);
+
+  if (!details) {
+    return res.status(404).render("errors/404", {
+      title: "Route Not Found",
+      error: "The route you are looking for does not exist.",
+    });
+  }
 
   res.render("routes/details", {
     title: "Route Details",
